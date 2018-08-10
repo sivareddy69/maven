18ac99fe05e9275bb367a5a001f26dc53e7c5166
gopi <mvsivareddy469@gmail.com>
2018-08-10 12:14:12 +0530

    ecom project commit

---
+++ src/main/java/ecoms1/project1.java
@@ -0,0 +1,8 @@
+package ecoms1;
+
+public class project1 {
+	public String sample() {
+		return "sample program";
+	}
+
+}
---
+++ src/test/java/ecoms1/projecttest.java
@@ -0,0 +1,14 @@
+package ecoms1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class projecttest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
